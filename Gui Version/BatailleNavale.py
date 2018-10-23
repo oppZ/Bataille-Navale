634f5ad1e7da3c1de9acb280c0741f4749d0e5b7
opp <mateusz.csi@gmail.com>
2018-10-23 14:41:38 +0200

    Update
    Add : Création du tableau de l'utilisateur, qui n'est pas encore fonctionnel

--- Gui Version/BatailleNavale.py
+++ Gui Version/BatailleNavale.py
@@ -18,91 +18,173 @@ ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 NUMBER_DEFAULT_SHIPS_PER_LENGTH = {1:0, 2:1, 3:2, 4:1, 5:1} 
 NUMBER_SHIPS_PER_LENGTH = {}
 
+MODE = 0 #Par défaut, mode SOLO
+
 player1Tab = []
 computerTab, player2Tab = [], []
 
 #Fonction qui demande la longueur du bateau et l'id pour pouvoir le placer sur le terrain de jeu de l'ordinateur
 
-def computerPlacement(shipLength, shipId):
+def placement(shipLength, shipId, x, y, playerTab):
+    global ship
 
+    tab = computerTab if MODE == 0 else playerTab 
+    
+    for case in range(shipLength -1):
+        if direction == 1 and y > 0:
+            #HAUT
+            y = y - 1
+            ship.append(str(x) + " " + str(y))
+
+        elif direction == 2 and y < LINES - 1:
+            #BAS
+            y = y + 1
+            ship.append(str(x) + " " + str(y))
+
+        elif direction == 3 and x > 0:
+            #GAUCHE
+            x = x - 1
+            ship.append(str(x) + " " + str(y))
+
+        elif direction == 4 and x < COLUMNS -1:
+            #DROITE
+            x = x + 1
+            ship.append(str(x) + " " + str(y))
+        else:
+            return -1
+        '''
+        Si on sort des limites du jeu, on sort de la fonction
+        et on retourne 'erreur'
+        '''
+        
+        '''
+        Si la case est occupée, on sort de la fonction
+        et on retourne 'erreur'
+        '''
+        if tab[x][y] != 0:
+            return -1
+
+    '''
+    Si on arrive ici, c'est que toutes les positions
+    existent et ne sont pas déjà prises
+    '''
+    for case in range(len(ship)):
+        t = ship[case].split(" ")
+        tab[int(t[0])][int(t[1])] = shipId
+    return 0
+                        
+#Initialisation de la grille de l'ordinateur
+def computerPlacement(shipLength, shipId):
     x = randint(0, COLUMNS-1)
     y = randint(0, LINES-1)
-    global computerTab
+
+    global ship
     ship = []
-    #On regarde si la case d'origine est ocupé ou non. Si oui, on sort de la fonction
+	
     if computerTab[x][y] != 0:
         return -1
     else:
-        ship.append(str(x)+" "+str(y)) 
+        ship.append(str(x) + " " + str(y))
+
+    global direction
     direction = randint(1, 4)
-    for case in range(shipLength -1):
-        #UP
-        if direction == 1 and y > 0:
-            y = y - 1
-            ship.append(str(x)+" "+str(y))
-        #DOWN
-        elif direction == 2 and y < LINES - 1:
-            y = y + 1
-            ship.append(str(x)+" "+str(y))
-        #LEFT
-        elif direction == 3 and x > 0:
-            x = x - 1
-            ship.append(str(x)+" "+str(y))
-        #RIGHT
-        elif direction == 4 and x < COLUMNS -1:
-            x = x + 1
-            ship.append(str(x)+" "+str(y))
-        #Si on sort des limites du tableau de jeu, alors on sort de cette fonction en retournant erreur
-        else:
-            return -1
-        #Si cettte case est occupé, alors on sort de cette fonction en retournant erreur
-        if computerTab[x][y] != 0:
-            return -1
-    #Si on arrive ici, alors toutes les positions sont libres et elles existent
-    print(ship,shipId)
-    for case in range(len(ship)):
-        xy = ship[case]
-        tab = xy.split(" ")
-        computerTab[int(tab[0])][int(tab[1])] = shipId
-    return 0
+
+    return placement(shipLength, shipId, x, y, -1)
+
+#Initialisation de la grille d'un joueur
+def playerPlacement(shipLength, shipId, playerTab):
+    x = randint(0, COLUMNS-1)
+    y = randint(0, LINES-1)
+    #TODO : Demander les coordonnées à l'utilisateur
+    
+    global ship
+    ship = []
+    if playerTab[x][y] != 0:
+        return -1
+    else:
+        ship.append(str(x) + "  " + str(y))
+
+    global direction
+    #TODO : Demander la direction à l'utilisateur
+    
+    
+    return placement(shipLength, shipId, x, y, playerTab)
+
+#Initialisation d'une grille
+def init_grid(isPlayer, playerTab):
+    shipId = 1
+    
+    #Ajout de bateaux sur le terrain de l'ordinateur
+    for shipLength in NUMBER_SHIPS_PER_LENGTH:
+        #Pour chaque bateau d'un taille NUMBER_SHIPS_PER_LENGTH[shipLength]
+        for ship in range(NUMBER_SHIPS_PER_LENGTH[shipLength]):
+            result = -1
+            #Tant qu'on ne ressoit pas le succčs de la fonction computerPlacement(), continuer
+            while result == -1 and NUMBER_SHIPS_PER_LENGTH[shipLength] != 0:                
+                if isPlayer == True:
+                    result = int(playerPlacement(shipLength, shipId, playerTab))
+                else:
+                    result = int(computerPlacement(shipLength, shipId))
+                if (result != -1):
+                    shipId = shipId + 1
 
 def new_game(isMulti):
     print("Création d'une nouvelle partie...")
+
+    global MODE
+    MODE = isMulti
+    
     global LINES, COLUMNS, NUMBER_SHIPS_PER_LENGTH
-    #Si les lignes, colones et le nombre de bateaux n'ont pas été changés dans les paramčtres, alors on récupčre les valeurs par défault
+    
+    '''
+    Si les paramètres n'ont pas été modifié,
+    alors on utilise ceux par défauts
+    '''
     if LINES == 0:
         LINES = deepcopy(NUMBER_DEFAULT_LINES)
     if COLUMNS == 0:
         COLUMNS = deepcopy(NUMBER_DEFAULT_COLUMNS)
     if len(NUMBER_SHIPS_PER_LENGTH) == 0:
         NUMBER_SHIPS_PER_LENGTH = deepcopy(NUMBER_DEFAULT_SHIPS_PER_LENGTH)
+    
+    print("[1/3] Initialisation de la grille du joueur 1")
 
-    #Si on veut jouer contre l'ordinateur
+    global player1Tab
+    
+    #Création de la grille du joueur 1
+    player1Tab = [0]*LINES
+    for _ in range(LINES):
+        player1Tab[_] = [0]*COLUMNS
+        
+    init_grid(True, player1Tab)
+
     if not(isMulti):
-        print("[1/3] Initialisation de la grille de l'ordinateur")
+        #====Solo====#
+        
+        print("[2/3] Initialisation de la grille de l'ordinateur")
 
         global computerTab
+        
         #Création du tableau de l'ordinateur
         computerTab = [0]*LINES
         for _ in range(LINES):
             computerTab[_] = [0]*COLUMNS
-        shipId = 1
-        #Ajout de bateaux sur le terrain de l'ordinateur
-        for shipLength in NUMBER_SHIPS_PER_LENGTH:
-            #Pour chaque bateau d'un taille NUMBER_SHIPS_PER_LENGTH[shipLength]
-            for ship in range(NUMBER_SHIPS_PER_LENGTH[shipLength]):
-                result = -1
-                #Tant qu'on ne ressoit pas le succčs de la fonction computerPlacement(), continuer
-                while result == -1 and NUMBER_SHIPS_PER_LENGTH[shipLength] != 0:                
-                    result = int(computerPlacement(shipLength, shipId))
-                    if result != -1:
-                        shipId = shipId + 1
+
+        init_grid(False, -1)
+            
+    else:
+        #====Multi====#
         
-        print("[1/3] TERMINÉ !")
-        for y in range(LINES):
-            for x in range(COLUMNS):
-                print(computerTab[x][y], end="")
-            print("")
+        print("[2/3] Initialisation de la grille du joueur 2")
+
+        global player2Tab
+
+        #Création du tableau du joueur 2
+        player2Tab = [0]*LINES
+        for _ in range(LINES):
+            player2Tab[_] = [0]*COLUMNS
+            
+        init_grid(True, player2Tab)
                     
 def continue_game():
     #TODO
@@ -116,12 +198,6 @@ def informations():
     #TODO
     print()
 
-def solo():
-    new_game(False)
-
-def multi():
-    new_game(True)
-
 def main_menu():
     window = Tk()
     window.title("Bataille Navale")
@@ -133,8 +209,8 @@ def main_menu():
 
     menu1bis = Menu(menu1, tearoff=0)
     menu1.add_cascade(label="Nouvelle Partie", menu=menu1bis)
-    menu1bis.add_command(label="1 joueur", command=solo)
-    menu1bis.add_command(label="2 joueurs", command=multi)   
+    menu1bis.add_command(label="1 joueur", command=lambda: new_game(1))
+    menu1bis.add_command(label="2 joueurs", command=lambda: new_game(0))   
     menu1.add_command(label="Continuer Partie", command=continue_game)
 
     menu2 = Menu(menu, tearoff=0)
